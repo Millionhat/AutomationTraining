dc99a0f0894484803ce05e67f846f3c12f2fb14e
Millionhat <juanspalma@gmail.com>
2022-01-25 16:26:00 -0500

    Added WebDriverContainer comment

--- src/main/java/pages/WebDriverContainer.java
+++ src/main/java/pages/WebDriverContainer.java
@@ -2,6 +2,9 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Class in charge of storing the webDriver for an easy access to the Page clases
+ */
 public class WebDriverContainer {
 
   public static WebDriver driver;
