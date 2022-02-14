ef461ce437653ea6bd5f41708c63c957671dd1a7
Millionhat <juanspalma@gmail.com>
2022-02-14 14:26:22 -0500

    modified constructor and divided the driver creation into separate methods

--- src/main/java/pages/WebDriverContainer.java
+++ src/main/java/pages/WebDriverContainer.java
@@ -21,37 +21,50 @@ public class WebDriverContainer {
    */
   private WebDriverContainer() {
     String browser = System.getenv("browserName");
+    driver = createDriver(browser);
+  }
+
+  private WebDriver createDriver(String browser) {
     switch (browser) {
       case "chrome":
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        break;
+        return chromeDriverSetup();
 
       case "firefox":
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.addArguments("--no-sandbox");
-        firefoxOptions.addArguments("--disable-dev-shm-usage");
-        firefoxOptions.addArguments("--headless");
-        driver = new FirefoxDriver(firefoxOptions);
-        break;
+        return firefoxDriverSetup();
 
       case "edge":
-        WebDriverManager.edgedriver().setup();
-        EdgeOptions edgeOptions = new EdgeOptions();
-        edgeOptions.addArguments("--no-sandbox");
-        edgeOptions.addArguments("--disable-dev-shm-usage");
-        edgeOptions.addArguments("--headless");
-        driver = new EdgeDriver(edgeOptions);
-        break;
+        return edgeDriverSetup();
 
       default:
         throw new RuntimeException("Browser not defined correctly or doesnt exist");
     }
+  }
+
+  private WebDriver edgeDriverSetup() {
+    WebDriverManager.edgedriver().setup();
+    EdgeOptions edgeOptions = new EdgeOptions();
+    edgeOptions.addArguments("--no-sandbox");
+    edgeOptions.addArguments("--disable-dev-shm-usage");
+    edgeOptions.addArguments("--headless");
+    return new EdgeDriver(edgeOptions);
+  }
+
+  private WebDriver firefoxDriverSetup() {
+    WebDriverManager.firefoxdriver().setup();
+    FirefoxOptions firefoxOptions = new FirefoxOptions();
+    firefoxOptions.addArguments("--no-sandbox");
+    firefoxOptions.addArguments("--disable-dev-shm-usage");
+    firefoxOptions.addArguments("--headless");
+    return new FirefoxDriver(firefoxOptions);
+  }
+
+  private WebDriver chromeDriverSetup() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--headless");
+    return new ChromeDriver(options);
   }
 
   /**
